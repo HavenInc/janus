f3ce619a02f59c012b87b3cf939f3b1020474115
Benjamin Graner <benjamin@haveninc.com>
2016-09-21 11:43:41 -0700

    init

---
+++ client/js/controllers/todo.js
@@ -0,0 +1,41 @@
+// Copyright IBM Corp. 2015. All Rights Reserved.
+// Node module: loopback-example-angular
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+angular
+  .module('app')
+  .controller('TodoController', ['$scope', '$state', 'Todo', function($scope,
+      $state, Todo) {
+    $scope.todos = [];
+    function getTodos() {
+      Todo
+        .find()
+        .$promise
+        .then(function(results) {
+          $scope.todos = results;
+        });
+    }
+    getTodos();
+
+    $scope.addTodo = function() {
+      Todo
+        .create($scope.newTodo)
+        .$promise
+        .then(function(todo) {
+          $scope.newTodo = '';
+          $scope.todoForm.content.$setPristine();
+          $('.focus').focus();
+          getTodos();
+        });
+    };
+
+    $scope.removeTodo = function(item) {
+      Todo
+        .deleteById(item)
+        .$promise
+        .then(function() {
+          getTodos();
+        });
+    };
+  }]);
---
+++ tests/smoke.js
@@ -0,0 +1,11 @@
+// Copyright IBM Corp. 2015. All Rights Reserved.
+// Node module: loopback-example-angular
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+var test = require('tape');
+
+test('smoke test', function(t) {
+  t.plan(1);
+  t.equal(1, 1);
+});
