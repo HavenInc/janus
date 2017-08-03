7b239447166965ee103a2e02dc4ae4beb98d86a5
Manjeer Srujan <manjeer.y@target.com>
2017-08-03 15:27:58 +0530

    Adding unit test cases

--- common/models/portCall.js
+++ common/models/portCall.js
@@ -24,11 +24,11 @@ module.exports = function(PortCall) {
       /*
         Group by RouteID
       */
-      for(var i=0; i < calls.length; i++) {
-        if(routeIdPortCallsMap[calls[i].routeId] == null){
-          routeIdPortCallsMap[calls[i].routeId] = [];
-        }
-        routeIdPortCallsMap[calls[i].routeId].push(calls[i]);
+      for (var i = 0; i < calls.length; i++) {
+          if (routeIdPortCallsMap[calls[i].routeId] == null) {
+              routeIdPortCallsMap[calls[i].routeId] = [];
+          }
+          routeIdPortCallsMap[calls[i].routeId].push(calls[i]);
       }
 
       /*
@@ -36,48 +36,54 @@ module.exports = function(PortCall) {
       */
       var allRoutes = [];
       for (var routeId in routeIdPortCallsMap) {
-         for( i=0; i < routeIdPortCallsMap[routeId].length; i++) {
-            for(var j=i+1; j < routeIdPortCallsMap[routeId].length; j++) {
-              if(routeIdPortCallsMap[routeId][i].port !==
-                 routeIdPortCallsMap[routeId][j].port){
-                 var route={};
-                 route.vessel = routeIdPortCallsMap[routeId][i].vessel;
-                 route.from = routeIdPortCallsMap[routeId][i].port;
-                 route.to =  routeIdPortCallsMap[routeId][j].port;
-                  route.type = 'direct';
-                  route.etd = routeIdPortCallsMap[routeId][i].etd;
-                  route.eta = routeIdPortCallsMap[routeId][j].eta;
-                  route.routeId = routeId;
-                  allRoutes.push(route);
+          for (i = 0; i < routeIdPortCallsMap[routeId].length; i++) {
+              for (var j = i + 1;
+                   j < routeIdPortCallsMap[routeId].length;
+                   j++) {
+                  if (routeIdPortCallsMap[routeId][i].port !==
+                      routeIdPortCallsMap[routeId][j].port) {
+                      var route = {};
+                      route.vessel = routeIdPortCallsMap[routeId][i].vessel;
+                      route.from = routeIdPortCallsMap[routeId][i].port;
+                      route.to = routeIdPortCallsMap[routeId][j].port;
+                      route.type = 'direct';
+                      route.etd = routeIdPortCallsMap[routeId][i].etd;
+                      route.eta = routeIdPortCallsMap[routeId][j].eta;
+                      route.routeId = routeId;
+                      allRoutes.push(route);
+                  }
               }
-            }
-         }
+          }
       }
 
       /*
       If transit shipments are requested, do the below.
       */
-      if(incTrans){
-            /*
-              Group all the routes from each port.
-            */
-            var portRouteMap = [];
-              for( i=0; i < allRoutes.length; i++) {
-              if(portRouteMap[allRoutes[i].from] == null){
-                portRouteMap[allRoutes[i].from] = [];
+      if (incTrans) {
+          /*
+          Group all the routes from each port.
+          */
+          var portRouteMap = [];
+          for (i = 0; i < allRoutes.length; i++) {
+              if (portRouteMap[allRoutes[i].from] == null) {
+                  portRouteMap[allRoutes[i].from] = [];
               }
               portRouteMap[allRoutes[i].from].push(allRoutes[i]);
-            }
+          }
 
-            /*
-              Find out all transit shipments starting from each port.
-            */
+          /*
+            Find out all transit shipments starting from each port.
+          */
           for (var port in portRouteMap) {
-    var curRoutes = getTransitRoutes(null, port,portRouteMap, etd,eta);
-      for( i = 0; i<curRoutes.length;i++ ){
-          allRoutes.push(curRoutes[i]);
-      }
-            }
+              var curRoutes = getTransitRoutes(null,
+                                               port,
+                                               portRouteMap,
+                                               etd,
+                                               eta);
+              for (i = 0; i < curRoutes.length; i++) {
+                  allRoutes.push(curRoutes[i]);
+              }
+          }
       }
 
       return cb(null, allRoutes);
@@ -102,80 +108,80 @@ module.exports = function(PortCall) {
 
 };
 
- function getTransitRoutes(prevRoute, curPort, portRouteMap,etd, eta){
-    var curRoutes = [];
-    var transRoutes = [];
-    var routes = portRouteMap[curPort];
-    /*
-      If current port have no routes starting from it, return empty array
-    */
-    if(routes == null){
-      return [];
-    }
-    for(var i=0; i<routes.length; i++){
-    /*
-      if eta and etd is in required window
-      , vessels are same,
-      routes are diff then attach to prevRoute
-    */
-      if(routes[i].etd != null &&
-         routes[i].etd >= etd  &&
-         routes[i].eta != null &&
-         routes[i].eta <= eta  &&
-         (prevRoute === null || prevRoute.vessel === routes[i].vessel) &&
-         (prevRoute === null || prevRoute.routeId !== routes[i].routeId)){
-              curRoutes.push(routes[i]);
-              if(prevRoute !== null && prevRoute.from !== routes[i].to){
-                  var route = {};
-                  route.from = prevRoute.from;
-                  route.to =  routes[i].to;
-                  route.etd = prevRoute.etd;
-                  route.eta = routes[i].eta;
-                  route.type = 'transit';
-                  route.vessel = routes[i].vessel;
-                  if(routes[i].from === route.from){
-                      route.routeId = routes[i].routeId;
-                  } else{
-                      route.routeId = prevRoute.routeId +
-                                      '--('+routes[i].from+')->' +
-                                      routes[i].routeId;
-                  }
-                  transRoutes.push(route);
-              }
-      }
-    }
+function getTransitRoutes(prevRoute, curPort, portRouteMap, etd, eta) {
+   var curRoutes = [];
+   var transRoutes = [];
+   var routes = portRouteMap[curPort];
+   /*
+     If current port have no routes starting from it, return empty array
+   */
+   if (routes == null) {
+       return [];
+   }
+   for (var i = 0; i < routes.length; i++) {
+       /*
+         if eta and etd is in required window
+         , vessels are same,
+         routes are diff then attach to prevRoute
+       */
+       if (routes[i].etd != null &&
+           routes[i].etd >= etd &&
+           routes[i].eta != null &&
+           routes[i].eta <= eta &&
+           (prevRoute === null || prevRoute.vessel === routes[i].vessel) &&
+           (prevRoute === null || prevRoute.routeId !== routes[i].routeId)) {
+           curRoutes.push(routes[i]);
+           if (prevRoute !== null && prevRoute.from !== routes[i].to) {
+               var route = {};
+               route.from = prevRoute.from;
+               route.to = routes[i].to;
+               route.etd = prevRoute.etd;
+               route.eta = routes[i].eta;
+               route.type = 'transit';
+               route.vessel = routes[i].vessel;
+               if (routes[i].from === route.from) {
+                   route.routeId = routes[i].routeId;
+               } else {
+                   route.routeId = prevRoute.routeId +
+                       '--(' + routes[i].from + ')->' +
+                       routes[i].routeId;
+               }
+               transRoutes.push(route);
+           }
+       }
+   }
 
-    /*
-      For each matching routes from curPort,
-      call the same function recursively
-      by making curRoute as prevRoute.
-    */
+   /*
+     For each matching routes from curPort,
+     call the same function recursively
+     by making curRoute as prevRoute.
+   */
 
-    for( i=0; i< curRoutes.length; i++){
-      var nextLevelRoutes = getTransitRoutes(curRoutes[i],
-                                             curRoutes[i].to,
-                                             portRouteMap,
-                                             curRoutes[i].eta,
-                                             eta);
-      for(var j=0; j< nextLevelRoutes.length; j++){
-        if(curPort !== nextLevelRoutes[j].to){
-          var transRoute = {};
-          transRoute.from = curPort;
-          transRoute.to = nextLevelRoutes[j].to;
-          transRoute.etd = curRoutes[i].etd;
-          transRoute.eta = nextLevelRoutes[j].eta;
-          transRoute.vessel = nextLevelRoutes[j].vessel;
-          transRoute.type = 'transit';
-          if(transRoute.from === nextLevelRoutes[j].from){
-            transRoute.routeId = nextLevelRoutes[j].routeId;
-          } else{
-            transRoute.routeId = curRoutes[i].routeId +
-                                 '--('+nextLevelRoutes[j].from+')-->' +
-                                 nextLevelRoutes[j].routeId;
-          }
-          transRoutes.push(transRoute);
-        }
-      }
-    }
-    return transRoutes;
- }
+   for (i = 0; i < curRoutes.length; i++) {
+       var nextLevelRoutes = getTransitRoutes(curRoutes[i],
+           curRoutes[i].to,
+           portRouteMap,
+           curRoutes[i].eta,
+           eta);
+       for (var j = 0; j < nextLevelRoutes.length; j++) {
+           if (curPort !== nextLevelRoutes[j].to) {
+               var transRoute = {};
+               transRoute.from = curPort;
+               transRoute.to = nextLevelRoutes[j].to;
+               transRoute.etd = curRoutes[i].etd;
+               transRoute.eta = nextLevelRoutes[j].eta;
+               transRoute.vessel = nextLevelRoutes[j].vessel;
+               transRoute.type = 'transit';
+               if (transRoute.from === nextLevelRoutes[j].from) {
+                   transRoute.routeId = nextLevelRoutes[j].routeId;
+               } else {
+                   transRoute.routeId = curRoutes[i].routeId +
+                       '--(' + nextLevelRoutes[j].from + ')-->' +
+                       nextLevelRoutes[j].routeId;
+               }
+               transRoutes.push(transRoute);
+           }
+       }
+   }
+   return transRoutes;
+}
--- test/portCall.js
+++ test/portCall.js
@@ -1,44 +1,61 @@
 const assert = require('chai').assert;
-    superagent = require('superagent'),
+superagent = require('superagent'),
     app = require('../server/server');
+var chaifs = require('chai-fs');
+var chai = require('chai');
+chai.use(chaifs);
+var expect = chai.expect;
+var file = chaifs.file;
+
 
 describe('PortCalls model', function() {
-  var server;
+    var server;
 
-  beforeEach(function(done) {
-    server = app.listen(done);
-  });
+    beforeEach(function(done) {
+        server = app.listen(done);
+    });
 
-  afterEach(function(done) {
-    server.close(done);
-  });
+    afterEach(function(done) {
+        server.close(done);
+    });
 
-  it('Check non-transit shipping voyages', function(done) {
-    superagent
-      .post('http://localhost:3000/api/PortCalls/getRoutes')
-      .send({"etd": "2016-01-03T18:30:00.000Z", "eta": "2016-02-18T18:30:00.000Z", "incTrans": true})
-      .set('Accept', 'application/json')
-      .set('Content-Type', 'application/json')
-      .end(function(err, loginRes) {
-          if (err) { return done(err); }
-          assert.equal(loginRes.status, 200);
-          //console.log(loginRes.body);
-          done();
-      });
-  });
+    it('Check non-transit shipping voyages', function(done) {
+        superagent
+            .post('http://localhost:3000/api/PortCalls/getRoutes')
+            .send({
+                "etd": "2016-01-03T18:30:00.000Z",
+                "eta": "2016-02-18T18:30:00.000Z",
+                "incTrans": true
+            })
+            .set('Accept', 'application/json')
+            .set('Content-Type', 'application/json')
+            .end(function(err, getRoutesResponse) {
+                if (err) {
+                    return done(err);
+                }
+                assert.equal(getRoutesResponse.status, 200);
+                expect("test/testWithTransShipments.json").to.be.a.file().with.content(JSON.stringify(getRoutesResponse.body));
+                done();
+            });
+    });
 
-  it('Check transit shipping voyages', function(done) {
-      superagent
-        .post('http://localhost:3000/api/PortCalls/getRoutes')
-        .send({"etd": "2016-01-03T18:30:00.000Z", "eta": "2016-02-18T18:30:00.000Z"})
-        .set('Accept', 'application/json')
-        .set('Content-Type', 'application/json')
-        .end(function(err, loginRes) {
-            if (err) { return done(err); }
-            assert.equal(loginRes.status, 200);
-            //console.log(loginRes.body);
-            done();
-        });
+    it('Check transit shipping voyages', function(done) {
+        superagent
+            .post('http://localhost:3000/api/PortCalls/getRoutes')
+            .send({
+                "etd": "2016-01-03T18:30:00.000Z",
+                "eta": "2016-02-18T18:30:00.000Z"
+            })
+            .set('Accept', 'application/json')
+            .set('Content-Type', 'application/json')
+            .end(function(err, getRoutesResponse) {
+                if (err) {
+                    return done(err);
+                }
+                assert.equal(getRoutesResponse.status, 200);
+                expect("test/testWithoutTransShipments.json").to.be.a.file().with.content(JSON.stringify(getRoutesResponse.body));
+                done();
+            });
     });
 
 });
